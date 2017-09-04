fd9cd568c7d28582b16237ebbd1a62a97d17108d
Joel Cressy <joel@jtcressy.net>
2017-09-04 13:16:05 -0600

    attempt 4 at fixing tz settings

--- functions.php
+++ functions.php
@@ -68,7 +68,7 @@ function expired($slide){
 
 function isFuture($slide){
 	$today = new DateTime();
-	$today.date_default_timezone_set("America/Denver");
+	//$today.date_default_timezone_set("America/Denver");
 	if ($today <= new DateTime($slide['startDate']) ) {
 		return true;
 	}
